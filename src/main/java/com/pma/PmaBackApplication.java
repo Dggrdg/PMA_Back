7ae1df0b74a701e786e9a1871b20556c17f129ca
Dggrdg <kobe640670@gmail.com>
2025-10-14 10:20:52 +0800

    init

---
+++ src/main/java/com/pma/PmaBackApplication.java
@@ -0,0 +1,13 @@
+package com.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmaBackApplication.class, args);
+	}
+
+}
